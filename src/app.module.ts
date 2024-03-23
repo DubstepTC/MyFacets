78d71099ebf41f0dc79d51d79dde409c2adb8848
DubstepTC <dubsteptc@mail.ru>
2024-03-23 15:19:43 +0300

    user_table

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { ConfigModule } from './config.module';
 import { TypeOrmModule } from './db/typeorm.module';
+import { UserModule } from './entities/user/user.module'
 
 @Module({
   imports: [
     TypeOrmModule,
     ConfigModule,
+    UserModule
   ],
 })
 export class AppModule {}
