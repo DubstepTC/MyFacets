223ef4872cf03dcbdad43ad6464ac9b7f6993e7e
Lilia_LuSol <maria.bochkova.2003.moon@yandex.ru>
2024-03-29 12:34:44 +0300

    Review

---
+++ src/reviews/review.entity.ts
@@ -0,0 +1,19 @@
+// import { Review } from '@prisma/client';
+// import { ApiProperty } from '@nestjs/swagger';
+
+// export class ReviewEntity implements Review{
+//     @ApiProperty()
+//     id: number;
+  
+//     @ApiProperty()
+//     title: string;
+  
+//     @ApiProperty({ required: false, nullable: true })
+//     description: string | null;
+  
+//     @ApiProperty()
+//     body: string;
+  
+//     @ApiProperty()
+//     createdAt: Date;
+// }
