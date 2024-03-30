a144b7deab37c90eea3c63d161b649b8d326e7ce
DubstepTC <dubsteptc@mail.ru>
2024-03-30 12:22:51 +0300

    update 1.0.1

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { MailService } from './mail/mail.service';
 
 @Module({
   imports: [JwtModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, MailService],
 })
 export class AuthModule {}
